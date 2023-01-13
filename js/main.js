9d1767080104767a613fbe1bf50894f3d191dd6f
SilverWings47 <nasrisami17@gmail.com>
2023-01-13 17:25:54 +0100

    First commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-getAdvice();
+document.onload = showAdvice();
 
 const adviceBtn = document.querySelector('.advice-btn');
 const adviceId = document.querySelector('.advice-id');
 const adviceText = document.querySelector('.advice-text');
 
-adviceBtn.addEventListener('click', getAdvice);
+adviceBtn.addEventListener('click', showAdvice);
 
-async function getAdvice() {
+async function fetchAdvice() {
     const response = await fetch('https://api.adviceslip.com/advice');
     const advice = await response.json();
 
-    console.log(advice);
+    return advice;
+}
+
+async function showAdvice() {
+    const advice = await fetchAdvice();
 
     adviceId.textContent = `advice #${advice.slip.id}`;
     adviceText.textContent = `“${advice.slip.advice}”`;
